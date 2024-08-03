2e547e02214ad7e88fa343b11c5776d5f89bee24
heyethereum <heyethereum@gmail.com>
2024-08-03 21:39:28 +0800

    backup qr code type table

---
+++ sql/qr_code_types_202408032138.sql
@@ -0,0 +1,11 @@
+INSERT INTO safeqr.qr_code_types ("type",description,prefix,table_name) VALUES
+	 ('URL','Uniform Resource Locator','http://','url'),
+	 ('EMAIL','Email Address','mailto:','email'),
+	 ('PHONE','Phone Number','tel:','phone'),
+	 ('SMS','Short Message Service','smsto:','sms'),
+	 ('GEOLOCATION','Geographic Location','geo:','geolocation'),
+	 ('WIFI','Wireless Network Configuration','WIFI:','wifi'),
+	 ('BITCOIN','Cryptocurrency Address','bitcoin:','bitcoin'),
+	 ('VCARD','Virtual Contact File','BEGIN:VCARD','vcard'),
+	 ('URL','Secure Uniform Resource Locator','https://','url'),
+	 ('TEXT','Text','','text');
